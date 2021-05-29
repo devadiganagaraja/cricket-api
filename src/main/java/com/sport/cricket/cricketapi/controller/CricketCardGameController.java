e22b5ce751fb800c0ad85888a510b31e9ad6f9b8
devadiganagaraja <connect2devadigan@gmail.com>
2021-05-29 18:00:15 +0530

    added crosssite

--- src/main/java/com/sport/cricket/cricketapi/controller/CricketCardGameController.java
+++ src/main/java/com/sport/cricket/cricketapi/controller/CricketCardGameController.java
@@ -3,11 +3,9 @@ package com.sport.cricket.cricketapi.controller;
 import com.sport.cricket.cricketapi.domain.games.cricketcards.PlayerGameInfo;
 import com.sport.cricket.cricketapi.service.CricketCardGameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 public class CricketCardGameController {
 
